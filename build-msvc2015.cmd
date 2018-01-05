d497a8c8483c2f058181b082d0e4984602a416e7
Alvin Wong <alvinhochun@gmail.com>
2018-01-06 02:07:02 +0800

    Generate krita-nsis package

--- build-msvc2015.cmd
+++ build-msvc2015.cmd
@@ -7,6 +7,7 @@ set KRITASHELLEX_VERSION_STRING=dev
 ::set KRITASHELLEX_VERSION_STRING=v1.2.4
 set CMAKE_EXE=cmake.exe
 set MAKENSIS_EXE=C:\Program Files (x86)\NSIS\makensis.exe
+set SEVENZIP_EXE=%ProgramFiles%\7-Zip\7z.exe
 
 :: Do not modify for release builds! You can use "Debug" for debug builds.
 set BUILD_CONFIGURATION=Release
@@ -15,6 +16,8 @@ mkdir build
 mkdir build\x86
 mkdir build\x64
 mkdir output
+mkdir output\krita-nsis
+mkdir output\krita-nsis\include
 
 echo Building 32-bit shell extension...
 pushd build\x86
@@ -43,4 +46,33 @@ copy /y ..\COPYING_tinyxml2.txt .
 copy /y ..\COPYING_zlib.txt .
 copy /y ..\README.md README.txt
 "%CMAKE_EXE%" -E touch CHANGELOG.TODO
+
+:: Prepare Krita installer package
+copy /y kritashellex32.dll .\krita-nsis\
+copy /y kritashellex64.dll .\krita-nsis\
+type COPYING.txt > .\krita-nsis\COPYING.txt
+echo. >> .\krita-nsis\COPYING.txt
+echo ## libzip: >> .\krita-nsis\COPYING.txt
+type COPYING_libzip.txt >> .\krita-nsis\COPYING.txt
+echo. >> .\krita-nsis\COPYING.txt
+echo ## tinyxml2: >> .\krita-nsis\COPYING.txt
+type COPYING_tinyxml2.txt >> .\krita-nsis\COPYING.txt
+echo. >> .\krita-nsis\COPYING.txt
+echo ## zlib: >> .\krita-nsis\COPYING.txt
+type COPYING_zlib.txt >> .\krita-nsis\COPYING.txt
+copy /y ..\installer\nsis\installer_krita.nsi .\krita-nsis\
+copy /y ..\installer\nsis\krita_versions_detect.nsh .\krita-nsis\
+copy /y ..\installer\nsis\krita_shell_integration.nsh .\krita-nsis\
+copy /y ..\installer\nsis\include\FileExists2.nsh .\krita-nsis\include\
+copy /y ..\installer\nsis\include\IsFileInUse.nsh .\krita-nsis\include\
+copy /y ..\installer\nsis\krita.ico .\krita-nsis\
+copy /y ..\installer\nsis\kritafile.ico .\krita-nsis\
+copy /y ..\installer\nsis\license.rtf .\krita-nsis\
+copy /y ..\installer\nsis\license_gpl-3.0.rtf .\krita-nsis\
+
+del krita-nsis-!KRITASHELLEX_VERSION_STRING!.zip
+"%SEVENZIP_EXE%" a -tzip krita-nsis-!KRITASHELLEX_VERSION_STRING!.zip krita-nsis\
+
+echo.
+echo Update CHANGELOG file before releasing^!
 popd
